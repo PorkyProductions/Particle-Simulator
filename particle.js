3319da45659e1d4969cde483e013bce28d82b25f
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-08-26 09:17:54 -0400

    particles respawn

--- particle.js
+++ particle.js
@@ -90,9 +90,32 @@ update = () => {
     for (i = 0; i < atoms.length; i++) {
         draw(atoms[i].x, atoms[i].y, atoms[i].color, 5)
     }
+    shuffle(atoms);
+    var first = atoms.shift();
+    
+    first.x = randomX();
+    first.y = randomY();
+    atoms.push(first);
     requestAnimationFrame(update)
 }
 update();
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
 
 
 function UpdateSliders() {
