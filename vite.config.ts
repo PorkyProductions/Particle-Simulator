3bb421c2dcaf9854a23b88fff77488e440837d9a
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-11-11 15:59:22 -0500

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 export default defineConfig ({
     plugins: [],
-    base: "/ParticleSimulator/",
     server: {
         open: '/index.html',
         port: 3000,
